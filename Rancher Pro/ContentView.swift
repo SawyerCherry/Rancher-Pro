7bd276f562683704cc50ccb7d3f799f041abbe63
Sawyer <sawyeracherry@gmail.com>
2021-11-29 11:46:14 -0600

    initial commit

---
+++ Rancher Pro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rancher Pro
+//
+//  Created by Sawyer Cherry on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rancher Pro/Rancher_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rancher_ProApp.swift
+//  Rancher Pro
+//
+//  Created by Sawyer Cherry on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Rancher_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
